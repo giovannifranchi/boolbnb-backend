dce6374a257227b487773d32256eab51db0e7870
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 00:08:19 +0200

    Add: relation among models

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -9,4 +9,31 @@ class Apartment extends Model
 {
     use HasFactory;
 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+
+    public function views(){
+        return $this->hasMany(View::class);
+    }
+
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function plans(){
+        return $this->belongsToMany(Plan::class)->withTimestamps();
+    }
+
+    public function services(){
+        return $this->belongsToMany(Service::class)->withTimestamps();
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    public function apartments(){
+        return $this->belongsToMany(Apartment::class)->withTimestamps();
+    }
 }
