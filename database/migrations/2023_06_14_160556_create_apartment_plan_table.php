a8375e3b5068dce4ceee9e865bd8ce34dd72e1b4
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-14 18:10:13 +0200

    Add: apartment_plan pivot table

---
+++ database/migrations/2023_06_14_160556_create_apartment_plan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_plan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('plan_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_plan');
+    }
+};
