ad2669c5adb0dce1fe778e00f2e28394a4313d56
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-14 18:03:17 +0200

    Add: tags table

--- database/migrations/2023_06_14_160049_create_tags_table.php
+++ database/migrations/2023_06_14_160049_create_tags_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('services', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('icon_url');
+            $table->string('icon_path');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('services');
+        Schema::dropIfExists('tags');
     }
 };
