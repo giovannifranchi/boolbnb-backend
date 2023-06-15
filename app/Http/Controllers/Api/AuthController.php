e36c4ff376894f1cd764ca81b97260450d5724be
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-16 00:38:52 +0200

    Add: login function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -28,5 +30,27 @@ class AuthController extends Controller
         ];
 
         return response($response, 201);
+    }
+
+    public function login(LoginRequest $request){
+        
+        $fields = $request->validated();
+
+        $user = User::where('email', $fields['email']);
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message'=>'invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        $response = [
+            'user'=>$user,
+            'token'=>$token
+        ];
+
+        return response($response, 200);
     }
 }
