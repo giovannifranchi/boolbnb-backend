e5f85e49f5fe4d4e3dd0ea4a2366661d82982ca1
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 15:47:12 +0200

    Add: service list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\View;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ApartmentSeeder::class,
             PlanSeeder::class,
+            ViewSeeder::class,
         ]);
     }
 }
