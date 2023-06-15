faa0a633c6822dd0e5f7f7a1c607ab288aac9c99
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 17:12:59 +0200

    Add: message seeder

---
+++ database/seeders/MessageSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apartment;
+use App\Models\Message;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i < 50; $i++){
+            $randomApartment = Apartment::inRandomOrder()->first();
+            $newMessage = new Message();
+
+            $newMessage->name = fake()->firstName();
+            $newMessage->lastname = fake()->lastName();
+            $newMessage->email = fake()->email();
+            $newMessage->text = fake()->text();
+            $newMessage->apartment_id = $randomApartment->id;
+
+            $newMessage->save();
+        }
+    }
+}
