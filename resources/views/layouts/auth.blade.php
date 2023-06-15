51b21bbeab4ecb377aee0402ccb6d5b79fe0ddf9
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 09:44:16 +0200

    Fix: comment out connection to projects

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -50,9 +50,9 @@
                                 <li class="nav-item">
                                     <a href="{{route('admin.dashboard')}}" class="nav-link">{{__('Dashboard')}}</a>
                                 </li>
-                                <li class="nav-item">
+                                {{-- <li class="nav-item">
                                     <a href="{{route('admin.projects.index')}}" class="nav-link">{{__('Projects')}}</a>
-                                </li>
+                                </li> --}}
                             @endauth
                         </ul>
 
