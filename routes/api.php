8c84c4e35c5ff74d5491289134420b2f8141d33c
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-16 18:52:56 +0200

    Add: services route api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\Public\ApartmentsController;
 
 use App\Http\Controllers\Api\Private\ApartmentController;
+use App\Http\Controllers\Api\Public\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,6 +32,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/apartments', [ApartmentsController::class, 'index']);
 Route::get('/apartments/highlighted', [ApartmentsController::class, 'highlighted']);
 Route::get('/apartments/{id}', [ApartmentsController::class, 'show']);
+Route::get('services', [ServiceController::class, 'index']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
