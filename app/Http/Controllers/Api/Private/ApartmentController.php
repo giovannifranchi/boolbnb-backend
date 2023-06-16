d36c8a89ff822c001f551a7a498ab6e65711e2e6
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-16 15:02:22 +0200

    Add: apartment update

--- app/Http/Controllers/Api/Private/ApartmentController.php
+++ app/Http/Controllers/Api/Private/ApartmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Private;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApartmentStoreRequest;
+use App\Http\Requests\Api\ApartmentUpdateRequest;
 use App\Models\Apartment;
 use App\Models\User;
 use Illuminate\Http\Client\ResponseSequence;
@@ -86,9 +87,36 @@ class ApartmentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $slug)
+    public function update(ApartmentUpdateRequest $request)
     {
-        //
+        $fields = $request->validated();
+        $user = $request->user();
+
+        $apartment = Apartment::where('user_id', $user->id)->where('slug', $fields['slug'])->first();
+
+        if(!$apartment){
+            return response(['error'=> 'apartment not found'], 404);
+        }
+
+        if($apartment->address !== $fields['address'] || $apartment->city !== $fields['city'] || $apartment->state !== $fields['state']){
+            $response = Http::get('https://api.tomtom.com/search/2/geocode/' . urlencode($fields['address'] . ', ' . $fields['city'] . ', ' . $fields['state']) . '.json', [
+                'key' => env('TOM_TOM_KEY')
+            ]);
+    
+            $data = $response->json();
+            if (!empty($data['results']) && isset($data['results'][0]['position'])) {
+                $apartment->latitude = $data['results'][0]['position']['lat'];
+                $apartment->longitude = $data['results'][0]['position']['lon'];
+            }else {
+                return response(['error'=>'internal service error'], 500);
+            }
+        }
+
+        if($apartment->name !== $fields['name']){
+            $apartment->slug = Str::slug($fields['name']);
+        }
+
+        $apartment->update($fields);
     }
 
     /**
