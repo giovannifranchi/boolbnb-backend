5294d1804de496ce60d2f00110b36d498607a271
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-19 12:35:49 +0200

    Add: start associative array

--- config/images.php
+++ config/images.php
@@ -5,40 +5,5 @@ return [
         'id'=> 1,
         'path'=>'/storage/images/Amsterdam/amsterdam.jpg'
     ],
-    [
-        'id'=> 2,
-        'path'=>'/storage/images/Barcellona/barcellona.jpg'
-    ],
-    [
-        'id'=> 3,
-        'path'=>'/storage/images/Berlino/Berlino.webp'
-    ],
-    [
-        'id'=> 4,
-        'path'=>'/storage/images/Berna/berna.jpg'
-    ],
-    [
-        'id'=> 5,
-        'path'=>'/storage/images/Bruxelles/bruxelles.jpg'
-    ],
-    [
-        'id'=> 6,
-        'path'=>'/storage/images/Budapest/Budapest.jpg'
-    ],
-    [
-        'id'=> 7,
-        'path'=>'/storage/images/Copenaghen/copenaghen.jpg'
-    ],
-    [
-        'id'=> 8,
-        'path'=>'/storage/images/Dublino/dublino.jpg'
-    ],
-    [
-        'id'=> 9,
-        'path'=>'/storage/images/Roma/roma.webp'
-    ],
-    [
-        'id'=> 10,
-        'path'=>'/storage/images/Parigi/parigi.jpg'
-    ],
+    
 ];
