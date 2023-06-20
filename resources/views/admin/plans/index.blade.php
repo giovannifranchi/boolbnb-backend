c21ab44a2f9174c398195f25fb742fbadef1f89e
P4anix <pancok_1@yahoo.it>
2023-06-20 18:07:21 +0200

    add: plans view

--- resources/views/admin/plans/index.blade.php
+++ resources/views/admin/plans/index.blade.php
@@ -1,61 +1,22 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="container">
-        <h1 class="my-3">Plans List</h1>
+<div class="container text-center">
+    <h1 class="my-3">Plans List</h1>
 
-        <a href="{{ route('admin.plans.create') }}" class="btn btn-success mb-3">
-            <h5 class="mb-0">Add a new plan</h5>
-        </a>
-
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Duration</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($plans as $plan)
-                    <tr>
-                        <td>{{ $plan->id }}</td>
-                        <td>{{ $plan->name }}</td>
-                        <td>{{ $plan->duration }} hours</td>
-                        <td>{{ $plan->price }} €</td>
-                        <td class="d-flex gap-3">
-                            <a href="{{ route('admin.plans.edit', $plan)}}" class="btn btn-warning">Edit</a>
-                            <button class="btn btn-danger" data-bs-toggle="modal"
-                                data-bs-target="#modal{{ $plan->id }}">Delete</button>
-                        </td>
-                    </tr>
-                    {{-- modal --}}
-                    <div class="modal" tabindex="-1" id="modal{{ $plan->id }}">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">You are deleting plan #{{ $plan->id }}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Are you sure you want to delete this plan?</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <form action="{{ route('admin.plans.destroy', $plan) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="row justify-content-center">
+        @foreach ($plans as $plan)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $plan->name }}</h6>
+                    <h5 class="card-title">{{ $plan->duration }} hours</h5>
+                    <p class="card-text">{{ $plan->price }} €</p>
+                    <a href="#" class="card-link btn btn-primary">Paga!!</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
+</div>
 @endsection
