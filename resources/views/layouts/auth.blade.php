9e29ecec676daefb7269882ad4e37c690c0c5031
P4anix <pancok_1@yahoo.it>
2023-06-21 09:49:59 +0200

    add: button payment

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -55,16 +55,16 @@
                                 </li>
 
 
-                                <li class="nav-item">
+                                {{-- <li class="nav-item">
                                     <a href="{{route('admin.services.index')}}" class="nav-link">{{__('Services')}}</a>
                                 </li> 
 
                                 <li class="nav-item">
                                     <a href="{{route('admin.tags.index')}}" class="nav-link">{{__('Tags')}}</a>
-                                </li>
+                                </li> 
                                 <li class="nav-item">
                                     <a href="{{route('admin.plans.index')}}" class="nav-link">{{__('Plans')}}</a>
-                                </li>
+                                </li> --}}
 
                             @endauth
                         </ul>
