8c1c16e0dac4334154a772a017c7bf33283c4fe3
P4anix <pancok_1@yahoo.it>
2023-06-21 17:19:08 +0200

    add: super_plus_for_me

--- resources/views/admin/gallery/index.blade.php
+++ resources/views/admin/gallery/index.blade.php
@@ -3,14 +3,23 @@
 @section('content')
 <main>
     <div class="container">
-
-        <div class="row">
+        <div class="row gy-3">
             @foreach ($images as $image)
-            <div class="col-6">
-                <img src="{{asset($image->path)}}" alt="ciao ciao" class="img-fluid">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="{{ asset($image->path) }}" class="img-fluid" style="width: 100%; height: 400px;" alt="...">
+                </div>
+            </div>
+            @endforeach
+            <div class="col-md-4" style="height: 100vh;">
+                <div class="card">
+                    <input type="file" name="fileInput" id="fileInput" style="display: none;">
+                    <label for="fileInput" class="site-btn d-flex align-items-center justify-content-center" style="width: 100%; height: 400px;">
+                        <h1>+</h1>
+                    </label>
+                </div>
             </div>
 
-            @endforeach
         </div>
     </div>
 </main>
