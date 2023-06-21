9d656b1563caa2eda5134d8d925f417bc6e6035e
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-21 19:35:10 +0200

    Add: single file upload for api working fully

--- app/Http/Controllers/Api/Private/UploadController.php
+++ app/Http/Controllers/Api/Private/UploadController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers\Api\Private;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
-    public function upload(Request $request){
-
-        return response()->json([
-            'message'=>'ok',
-        ]);
+    public function upload(Request $request)
+    {
+        if($request->hasFile('image')){
+            try {
+                $file = $request->file('image');
+                $path = $file->store('images', 'public');
+                
+                return response()->json([
+                    'message'=>'file uploaded successfully',
+                    'path' => asset("storage/".$path),
+                ], 200);
+                
+            } catch(\Exception $e) {
+                return response()->json([
+                    'message'=>$e->getMessage()
+                ]);
+            }
+        }
     }
+     
+    
 }
