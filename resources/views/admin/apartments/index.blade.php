e6cf4fa502b7b99a32bd1b020403914c61207299
P4anix <pancok_1@yahoo.it>
2023-06-24 17:58:55 +0200

    Add: show card

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -6,7 +6,7 @@
 
 
     <div class="projcard-container py-3">
-        <button class="btn btn-success mb-3 rounded fs-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">+</button>
+        <button class="btn btn-success mb-3 radius-50 fs-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">+</button>
         @foreach ($apartments as $apartment )
         <div class="projcard projcard-blue">
             <div class="projcard-innerbox">
@@ -18,6 +18,24 @@
                     <div class="projcard-description d-flex justify-content-center flex-column">
                         <h4><strong>LOCATION:</strong> {{$apartment->address}}, {{$apartment->city}}, {{$apartment->state}}</h4>
                         <h6><strong>PRICE:</strong> {{$apartment->price}} €</h6>
+                        <div class="d-flex gap-3">
+                            <div>
+                                <h6 class="d-flex gap-2 align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z" />
+                                    </svg>
+                                    <span>{{count($apartment->views)}}</span>
+                                </h6>
+                            </div>
+                            <div>
+                                <h6 class="d-flex gap-2 align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <path d="M160 368c26.5 0 48 21.5 48 48v16l72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6H448c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64c-8.8 0-16 7.2-16 16V352c0 8.8 7.2 16 16 16h96zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3V474.7v-6.4V468v-4V416H112 64c-35.3 0-64-28.7-64-64V64C0 28.7 28.7 0 64 0H448c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H309.3L208 492z" />
+                                    </svg>
+                                    <span>{{count($apartment->messages)}}</span>
+                                </h6>
+                            </div>
+                        </div>
                     </div>
                     <div class="projcard-tagbox d-flex">
                         <a href="{{ route('admin.apartments.show', $apartment)}}" class="projcard-tag text-decoration-none"><strong>DETAILS</strong></a>
@@ -38,118 +56,118 @@
     <!-- Offcanvas -->
 
     <!-- TODO: FIX offcanvas with media queries to match w-100 when mobile -->
-    <div class="offcanvas offcanvas-end w-25" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+    <div class="offcanvas offcanvas-end w-25 {{$errors->any() ? 'show' : ''}} ms-offcanva" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Create New Apartment</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn-close text-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-        @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    <form action="{{ route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-
-        {{-- inserimento nome appartamento --}}
-        <div class="mb-3">
-            <label for="name" class="form-label">Apartment Name</label>
-            <input type="text" class="form-control" id="name" value="{{ old('name')}}" name="name">
-        </div>
-        {{-- inserimento indirizzo --}}
-        <div class="mb-3">
-            <label for="address-input" class="form-label">Address</label>
-            <input type="text" class="form-control" id="address-input" value="{{ old('address')}}">
-        </div>
-        {{-- lista dinamica di autocompletamento --}}
-        <ul class="list-unstyled d-none" id="autocompleteContainer"></ul>
-        <input type="text" name="address" id="address" class="d-none">
-        {{-- inserimento città --}}
-        <div class="mb-3 d-none">
-            <label for="city" class="form-label">City</label>
-            <input type="text" class="form-control" id="city" value="{{old('city')}}" name="city">
-        </div>
-        {{-- inserimento nazione  --}}
-        <div class="mb-3 d-none">
-            <label for="state" class="form-label">State</label>
-            <input type="text" class="form-control" id="state" value="{{old('state')}}" name="state">
-        </div>
-        {{-- inserimento metri quadri appartamento  --}}
-        <div class="mb-3">
-            <label for="square_meters" class="form-label">Square Meters</label>
-            <input type="number" class="form-control" id="square_meters" value="{{old('square_meters')}}" name="square_meters" path="^(?!-)[0-9]+$">
-        </div>
-        {{-- inserimento numero bagni --}}
-        <div class="mb-3">
-            <label for="bathrooms" class="form-label">Bathrooms Number</label>
-            <input type="number" step="0.01" class="form-control" id="bathrooms" value="{{old('bathrooms')}}" name="bathrooms" path="^(?!-)[0-9]+$">
-        </div>
-        {{-- inserimento numero stanze --}}
-        <div class="mb-3">
-            <label for="rooms" class="form-label">Rooms Number</label>
-            <input type="number" class="form-control" id="rooms" value="{{old('rooms')}}" name="rooms" path="^(?!-)[0-9]+$">
-        </div>
-        {{-- inserimento numero letti --}}
-        <div class="mb-3">
-            <label for="beds" class="form-label">Beds Number</label>
-            <input type="number" class="form-control" id="beds" value="{{old('beds')}}" name="beds" path="^(?!-)[0-9]+$">
-        </div>
-        {{-- inserimento prezzo appartamento  --}}
-        <div class="mb-3">
-            <label for="price" class="form-label">Price</label>
-            <input type="number" class="form-control" id="price" value="{{old('price')}}" name="price" path="^(?!-)[0-9]+$">
-        </div>
-        {{-- inserimento valore percentuale dello sconto --}}
-        <div class="mb-3">
-            <label for="discount" class="form-label">Discount Value</label>
-            <input type="number" class="form-control" id="discount" value="{{old('discount')}}" name="discount" path="^(?!-)[0-9]+$">
-        </div>
-    
-        <div class="mb-3">
-            <div>Services</div>
-            @foreach ($services as $service)
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" name="services[]" id="services" value="{{ $service->id }}" {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}>
-                <label class="form-check-label" for="services">{{ $service->name }}</label>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-            @endforeach
-        </div>
+            @endif
+            <form action="{{ route('admin.apartments.store')}}" method="POST" enctype="multipart/form-data">
+                @csrf
 
-        {{-- inserimento descrizione appartamento  --}}
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" id="description" rows="3" name="description">{{old('description')}}</textarea>
-        </div>
+                {{-- inserimento nome appartamento --}}
+                <div class="mb-3">
+                    <label for="name" class="form-label">Apartment Name</label>
+                    <input type="text" class="form-control" id="name" value="{{ old('name')}}" name="name">
+                </div>
+                {{-- inserimento indirizzo --}}
+                <div class="mb-3">
+                    <label for="address-input" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="address-input" value="{{ old('address')}}">
+                </div>
+                {{-- lista dinamica di autocompletamento --}}
+                <ul class="list-unstyled d-none" id="autocompleteContainer"></ul>
+                <input type="text" name="address" id="address" class="d-none">
+                {{-- inserimento città --}}
+                <div class="mb-3 d-none">
+                    <label for="city" class="form-label">City</label>
+                    <input type="text" class="form-control" id="city" value="{{old('city')}}" name="city">
+                </div>
+                {{-- inserimento nazione  --}}
+                <div class="mb-3 d-none">
+                    <label for="state" class="form-label">State</label>
+                    <input type="text" class="form-control" id="state" value="{{old('state')}}" name="state">
+                </div>
+                {{-- inserimento metri quadri appartamento  --}}
+                <div class="mb-3">
+                    <label for="square_meters" class="form-label">Square Meters</label>
+                    <input type="number" class="form-control" id="square_meters" value="{{old('square_meters')}}" name="square_meters" path="^(?!-)[0-9]+$">
+                </div>
+                {{-- inserimento numero bagni --}}
+                <div class="mb-3">
+                    <label for="bathrooms" class="form-label">Bathrooms Number</label>
+                    <input type="number" step="0.01" class="form-control" id="bathrooms" value="{{old('bathrooms')}}" name="bathrooms" path="^(?!-)[0-9]+$">
+                </div>
+                {{-- inserimento numero stanze --}}
+                <div class="mb-3">
+                    <label for="rooms" class="form-label">Rooms Number</label>
+                    <input type="number" class="form-control" id="rooms" value="{{old('rooms')}}" name="rooms" path="^(?!-)[0-9]+$">
+                </div>
+                {{-- inserimento numero letti --}}
+                <div class="mb-3">
+                    <label for="beds" class="form-label">Beds Number</label>
+                    <input type="number" class="form-control" id="beds" value="{{old('beds')}}" name="beds" path="^(?!-)[0-9]+$">
+                </div>
+                {{-- inserimento prezzo appartamento  --}}
+                <div class="mb-3">
+                    <label for="price" class="form-label">Price</label>
+                    <input type="number" class="form-control" id="price" value="{{old('price')}}" name="price" path="^(?!-)[0-9]+$">
+                </div>
+                {{-- inserimento valore percentuale dello sconto --}}
+                <div class="mb-3">
+                    <label for="discount" class="form-label">Discount Value</label>
+                    <input type="number" class="form-control" id="discount" value="{{old('discount')}}" name="discount" path="^(?!-)[0-9]+$">
+                </div>
 
-        {{-- Longitude --}}
-        <input type="text" name="longitude" id="longitude" class="d-none">
-        {{-- Latitude --}}
-        <input type="text" name="latitude" id="latitude" class="d-none">
-        
-        {{-- inserimento immagine che si vuole avere come copertina  --}}
-        <div class="mb-3">
-            <label for="cover_image" class="form-label">Cover Image</label>
-            <input class="form-control" type="file" id="cover_image" name="thumb" onchange="previewImage(event, 'file-image-preview')" multiple>
-            <div class="preview">
-                <img id="file-image-preview" class="img-fluid" style="width: 200px;">
-            </div>
-        </div>
-        {{-- inserimento immagini aggiuntive --}}
-        <div class="mb-3">
-            <label for="additional_images" class="form-label">Additional Images</label>
-            <input class="form-control" type="file" id="additional_images" name="additional_images[]" onchange="previewMultipleImages(event, 'additional-images-preview')" multiple>
-            <div class="preview" id="additional-images-preview" style="display: flex; width: 200px;"></div>
-        </div>
-        
-        <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </form>
+                <div class="mb-3">
+                    <div>Services</div>
+                    @foreach ($services as $service)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="services[]" id="services" value="{{ $service->id }}" {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="services">{{ $service->name }}</label>
+                    </div>
+                    @endforeach
+                </div>
+
+                {{-- inserimento descrizione appartamento  --}}
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" id="description" rows="3" name="description">{{old('description')}}</textarea>
+                </div>
+
+                {{-- Longitude --}}
+                <input type="text" name="longitude" id="longitude" class="d-none">
+                {{-- Latitude --}}
+                <input type="text" name="latitude" id="latitude" class="d-none">
+
+                {{-- inserimento immagine che si vuole avere come copertina  --}}
+                <div class="mb-3">
+                    <label for="cover_image" class="form-label">Cover Image</label>
+                    <input class="form-control" type="file" id="cover_image" name="thumb" onchange="previewImage(event, 'file-image-preview')" multiple>
+                    <div class="preview">
+                        <img id="file-image-preview" class="img-fluid" style="width: 200px;">
+                    </div>
+                </div>
+                {{-- inserimento immagini aggiuntive --}}
+                <div class="mb-3">
+                    <label for="additional_images" class="form-label">Additional Images</label>
+                    <input class="form-control" type="file" id="additional_images" name="additional_images[]" onchange="previewMultipleImages(event, 'additional-images-preview')" multiple>
+                    <div class="preview" id="additional-images-preview" style="display: flex; width: 200px;"></div>
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
 </main>
@@ -184,7 +202,10 @@
 
 <style>
     /* show style */
-
+    .ms-offcanva{
+        background-color: #252A34;
+        color: #EAEAEA;
+    }
 
     /* Actual Code: */
     .projcard-container,
