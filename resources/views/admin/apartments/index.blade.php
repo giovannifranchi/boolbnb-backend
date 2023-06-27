708cd6e64b487d2a375af14bdafc85ebb5e3693c
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-28 01:43:10 +0200

    add: client request

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -113,13 +113,12 @@
                     {{-- inserimento nome appartamento --}}
                     <div class="mb-3">
                         <label for="name" class="form-label">Apartment Name</label>
-                        <input type="text" class="form-control" id="name" value="{{ old('name') }}"
-                            name="name">
+                        <input type="text" class="form-control" id="name" value="{{ old('name') }}" name="name" minlength="3" required>
                     </div>
                     {{-- inserimento indirizzo --}}
                     <div class="mb-3">
                         <label for="address-input" class="form-label">Address</label>
-                        <input type="text" class="form-control" id="address-input" value="{{ old('address') }}">
+                        <input type="text" class="form-control" id="address-input" value="{{ old('address') }}" required>
                     </div>
                     {{-- lista dinamica di autocompletamento --}}
                     <ul class="list-unstyled d-none" id="autocompleteContainer"></ul>
@@ -140,37 +139,37 @@
                     <div class="mb-3">
                         <label for="square_meters" class="form-label">Square Meters</label>
                         <input type="number" class="form-control" id="square_meters"
-                            value="{{ old('square_meters') }}" name="square_meters" path="^(?!-)[0-9]+$">
+                            value="{{ old('square_meters') }}" name="square_meters" step="1" min="30" pattern="^(?!-)[0-9]+$" required>
                     </div>
                     {{-- inserimento numero bagni --}}
                     <div class="mb-3">
                         <label for="bathrooms" class="form-label">Bathrooms Number</label>
                         <input type="number" step="0.01" class="form-control" id="bathrooms"
-                            value="{{ old('bathrooms') }}" name="bathrooms" path="^(?!-)[0-9]+$">
+                            value="{{ old('bathrooms') }}" name="bathrooms" step="1" min="1" pattern="^(?!-)[0-9]+$" required>
                     </div>
                     {{-- inserimento numero stanze --}}
                     <div class="mb-3">
                         <label for="rooms" class="form-label">Rooms Number</label>
                         <input type="number" class="form-control" id="rooms" value="{{ old('rooms') }}"
-                            name="rooms" path="^(?!-)[0-9]+$">
+                            name="rooms" step="1" min="1" pattern="^(?!-)[0-9]+$" required>
                     </div>
                     {{-- inserimento numero letti --}}
                     <div class="mb-3">
                         <label for="beds" class="form-label">Beds Number</label>
                         <input type="number" class="form-control" id="beds" value="{{ old('beds') }}"
-                            name="beds" path="^(?!-)[0-9]+$">
+                            name="beds" step="1" min="1" pattern="^(?!-)[0-9]+$" required>
                     </div>
                     {{-- inserimento prezzo appartamento  --}}
                     <div class="mb-3">
                         <label for="price" class="form-label">Price</label>
                         <input type="number" class="form-control" id="price" value="{{ old('price') }}"
-                            name="price" path="^(?!-)[0-9]+$">
+                            name="price"min="1" step="0.5" pattern="^(?!-)[0-9]+$" required>
                     </div>
                     {{-- inserimento valore percentuale dello sconto --}}
                     <div class="mb-3">
                         <label for="discount" class="form-label">Discount Value</label>
                         <input type="number" class="form-control" id="discount" value="{{ old('discount') }}"
-                            name="discount" path="^(?!-)[0-9]+$">
+                            name="discount" step="1" min="0" max="100" pattern="^(?!-)[0-9]+$" required>
                     </div>
 
                     <div class="mb-3">
