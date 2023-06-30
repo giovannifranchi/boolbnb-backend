3cbb35d3401b10ae8226e28884fa5546cf0c8b2a
Herenyon <rikitur17@gmail.com>
2023-06-30 16:54:53 +0200

    modifica titolo pagina

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,8 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>My Area</title>
+{{-- {{ config('app.name', 'Laravel') }} questo va nel <title> --}}
     <link rel="icon" href="{{ URL::asset('favicon.svg') }}" type="image/x-icon" />
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
