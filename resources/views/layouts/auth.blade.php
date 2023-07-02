d9094e7f4a06e12073eb69540cb625fe9d265aba
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-07-02 17:24:53 +0200

    Add: fix to head

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BoolBnB') }}</title>
 
     <link rel="icon" href="{{ URL::asset('favicon.svg') }}" type="image/x-icon" />
     <!-- Fonts -->
